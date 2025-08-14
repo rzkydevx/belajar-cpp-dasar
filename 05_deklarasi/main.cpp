b18be6297801fb992d2e58c729239880eaffd18c
rzkydevx <rzkydevx@gmail.com>
2025-08-14 16:28:37 +0700

    feat: add variable and declaration chapters

---
+++ 05_deklarasi/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    a = 10;
+    cout << a << endl;
+
+    int b;
+    cout << "Enter a number: ";
+    cin >> b;
+    cout << b << endl;
+
+    cin.get();
+    return 0;
+}
