ec8d4789bdc1344f97d99d516c682131f400c930
rzkydevx <rzkydevx@gmail.com>
2025-08-16 18:51:53 +0700

    feat: add digital logic chapter

---
+++ 09_logika_digital/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 3;
+    int b = 2;
+
+    bool hasil;
+
+    // Not
+    hasil = !(a == 3);
+    cout << "Not : " << hasil << endl;
+
+    // And
+    hasil = (a == 3) && (b == 2);
+    cout << "And : " << hasil << endl;
+
+    // Or
+    hasil = (a == 3) || (b == 3);
+    cout << "Or : " << hasil << endl;
+
+    cin.get();
+    return 0;
+}
