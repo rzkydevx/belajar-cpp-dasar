e02dbaa814152aa7df8c5ad0822a05155857e5c3
rzkydevx <rzkydevx@gmail.com>
2025-08-21 19:37:23 +0700

    feat: add do while loop example

---
+++ 17_dowhile/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 1;
+
+    do
+    {
+        cout << "Nilai i: " << i << endl;
+    } while (i++ < 10);
+
+    cin.get();
+    return 0;
+}
