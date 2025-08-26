f8ab88a7be1138fcc33952b010f77eacabab6050
rzkydevx <rzkydevx@gmail.com>
2025-08-26 18:54:44 +0700

    feat: add break and continue example
    Adds a new C++ program to demonstrate the use of break and continue statements within a loop.
    
    This example shows how to skip an iteration with continue and how to exit a loop prematurely with break.

---
+++ 19_break_continue/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  for (int i = 0; i < 10; i++) {
+    if (i == 5) {
+      continue;
+    }
+
+    if (i == 8) {
+      break;
+    }
+
+    cout << i << endl;
+  }
+
+  cout << "End of The Program..." << endl;
+
+  cin.get();
+  return 0;
+}
