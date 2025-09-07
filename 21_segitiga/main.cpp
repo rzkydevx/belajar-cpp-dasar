b2918cbb5a0da837d2756d9dad4fa44405c9ef74
rzkydevx <rzkydevx@gmail.com>
2025-09-07 18:01:47 +0700

    feat(triangle): add more triangle patterns
    Adds three new triangle patterns (Pola 5, Pola 6, and Pola 7) to the existing program. These patterns demonstrate more complex loop structures to create different shapes.

--- 21_segitiga/main.cpp
+++ 21_segitiga/main.cpp
@@ -50,6 +50,52 @@ int main() {
     cout << endl;
   }
 
+  cout << "\nPola 5\n\n";
+
+  for (int i = 1; i <= n; i++) {
+    for (int j = n; j > i; j--) {
+      cout << " ";
+    }
+    for (int k = 1; k <= (2 * i - 1); k++) {
+      cout << "*";
+    }
+    cout << endl;
+  }
+
+  cout << "\nPola 6\n\n";
+
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j < i; j++) {
+      cout << " ";
+    }
+    for (int k = n; k >= (2 * i - n); k--) {
+      cout << "*";
+    }
+    cout << endl;
+  }
+
+  cout << "\nPola 7\n\n";
+
+  for (int i = 1; i <= n; i++) {
+    for (int j = n; j > i; j--) {
+      cout << " ";
+    }
+    for (int k = 1; k <= (2 * i - 1); k++) {
+      cout << "*";
+    }
+    cout << endl;
+  }
+
+  for (int i = 2; i <= n; i++) {
+    for (int j = 1; j < i; j++) {
+      cout << " ";
+    }
+    for (int k = n; k >= (2 * i - n); k--) {
+      cout << "*";
+    }
+    cout << endl;
+  }
+
   cin.get();
   return 0;
 }
